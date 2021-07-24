3acca006ddbc299cbe78cae771ef38b800a7173b
Easton Shaum <Eastonshaum@gmail.com>
2021-07-24 13:27:20 -0600

    inquirer now works for the first step

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES (‘Jeff’, ‘Bezos’, ‘1’),
+        (‘Steven’, ‘Trabajos’, 2, 1),
+        (‘Matt’, ‘Jones’, 2),
+        (‘Jeff’, ‘Jones’, 2),
+        (‘Jeff’, ‘Jones’, 3),
+        (‘Jeff’, ‘Jones’, 3),
+        (‘Jeff’, ‘Smith’, 3),
+        (‘Jeff’, ‘Smith’, 3),
+        (‘Jeff’, ‘Smith’, 3),
+        (‘Jeff’, ‘Smith’, ‘4’),
+        (‘Jeff’, ‘Smith’, ‘4’),
+        (‘Jeff’, ‘Smith’, ‘4’),;
--- index.js
+++ index.js
@@ -1,3 +1,70 @@
+const inquirer = require('inquirer')
 const db = require('./db/connection');
-const routes = require('./routes/');
+//const routes = require('./routes');
 
+
+const options = [
+    {
+        type: 'list',
+        name: 'initOptions',
+        message: 'Which would you like to view?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Quit']
+    }
+]
+
+function init(){
+    inquirer.prompt(options).then(res => {
+        console.log(res)
+        switch(res.initOptions) {
+            case ('View All Departments'): 
+                allDepartments();
+                break;
+            case ('View All Roles'):
+                allRoles();
+                break;
+            case ('View All Employees'):
+                allEmployees();
+                break;
+            case ('Add a Department'):
+                addDepartment();
+                break;
+            case ('Add a Role'):
+                addRole();
+                break;
+            case ('Quit'):
+                break;
+        }
+    })
+};
+
+function allDepartments() {
+    const departments = db.query()
+    console.table(departments)
+    init();
+};
+
+function allRoles() {
+    const roles = db.query()
+    console.table(roles)
+    init();
+};
+
+function allEmployees() {
+    const employees = db.query()
+    console.table(employees)
+    init();
+};
+
+function addDepartment() {
+    
+    init();
+};
+
+function addRole() {
+    
+    init();
+};
+
+
+
+init();
